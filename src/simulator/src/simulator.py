7f0db4d8ed428341f3c46a764d32b8e5c94234e7
2D-Car-Simulator <amsahasrabudhe@wpi.edu>
2018-11-10 12:30:28 -0500

    Added simulator config Added config loading from ros parameter server
    - Ashwin

--- src/simulator/src/simulator.py
+++ src/simulator/src/simulator.py
@@ -2,38 +2,67 @@
 
 import os
 import rospy
-import time
-
 import pygame
 
-WINDOW_WIDTH  = 1280
-WINDOW_HEIGHT = 720
+from simulator_config import SimulatorConfig
 
 class Simulator:
 
     def __init__(self):
 
-        pygame.init()
-        pygame.display.set_caption("2D Car Simulator")
+        self.sim_config = SimulatorConfig()
+        self.loadConfig( self.sim_config )
 
-        self.screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+        self.current_dir = os.path.dirname(os.path.abspath(__file__))
 
-        self.screen.fill((255,255,255))
+        self.setupSimulation()
 
-        current_dir = os.path.dirname(os.path.abspath(__file__))
-        image_path = os.path.join(current_dir, "resources/autonomous_car_small.png")
+        self.auto_car_image    = self.loadImage( self.sim_config.autonomous_car_image_file )
+        self.traffic_car_image = self.loadImage( self.sim_config.traffic_car_image_file )
 
-        self.car_image = pygame.image.load(image_path)
-
-        self.car_posX = 0
+        
 
         rospy.Timer(rospy.Duration(0.02), self.updateDisplay)
 
 
+    def loadConfig(self, cfg):
+
+        def loadParam(param_name, default):
+            return rospy.get_param("/simulator/"+param_name, default)
+
+        cfg.window_width               = loadParam("window_width", 1280)
+        cfg.window_height              = loadParam("window_height", 720)
+
+        cfg.env_yaml_file              = loadParam("env_yaml_file", "resources/environments/two_lane_straight_two_cars.yaml")
+        cfg.env_bg_image_file          = loadParam("env_bg_image_file", "resources/environments/two_lane_straight.png")
+
+        cfg.autonomous_car_image_file  = loadParam("autonomous_car_image_file", "resources/autonomous_car_small.png")
+        cfg.traffic_car_image_file     = loadParam("traffic_car_image_file", "resources/traffic_car_small.png")
+
+        cfg.display_update_duration_s  = loadParam("display_update_duration_s", 0.02)
+
+
+    def setupSimulation(self):
+
+        pygame.init()
+        pygame.display.set_caption("2D Car Simulator")
+
+        self.screen = pygame.display.set_mode((self.sim_config.window_width, self.sim_config.window_height))
+
+        self.bg_image = self.loadImage(self.sim_config.env_bg_image_file)
+        self.screen.blit(self.bg_image, (0, 0))
+
+
+    def loadImage(self, filename):
+
+        image_path = os.path.join(self.current_dir, filename)
+        return pygame.image.load(image_path).convert_alpha()
+
+
     def updateDisplay(self, event):
 
-        self.screen.fill((255, 255, 255))
-        self.screen.blit( pygame.transform.rotozoom(self.car_image, 0, 1), (self.car_posX,360) )
+        self.screen.blit(self.bg_image, (0, 0))
+        self.screen.blit( pygame.transform.rotozoom(self.auto_car_image, 0, 1), (self.car_posX,360) )
         self.car_posX += 5
 
         pygame.display.update()
---
+++ src/simulator/src/simulator_config.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+class SimulatorConfig:
+
+    def __init__(self):
+
+        self.window_width               = 1280
+        self.window_height              = 720
+
+        self.env_yaml_file              = "resources/environments/two_lane_straight_two_cars.yaml"
+        self.env_bg_image_file          = "resources/environments/two_lane_straight.png"
+
+        self.autonomous_car_image_file  = "resources/autonomous_car_small.png"
+        self.traffic_car_image_file     = "resources/traffic_car_small.png"
+
+        self.display_update_duration_s  = 0.02
